d417f1f4653922b82c6cccadfe68b61c808dd8ad
Alan Escobedo <ingescobedoalan4@gmail.com>
2023-04-04 20:19:06 +0000

    add public

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(fileUpload());
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-
+app.use(express.static("public"))
 app.get("/", (req, res) => {
 	res.status(200).json({
 		ok: true
